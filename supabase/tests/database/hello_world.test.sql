7a5dc27d6daa13f05b7742d2cb0feda28025e9ec
Restray <contact@restray.org>
2023-02-26 22:58:27 +0100

    fix(ci): name and config files

---
+++ supabase/tests/database/hello_world.test.sql
@@ -0,0 +1,17 @@
+begin;
+
+select plan(1);
+
+-- only one statement to run
+
+SELECT
+    has_column(
+        'auth',
+        'users',
+        'id',
+        'id should exist'
+    );
+
+select * from finish();
+
+rollback;
